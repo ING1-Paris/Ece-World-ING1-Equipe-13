d6fad95ac8aa5ca803686fbb62bc8f6d7e5d5612
LaCometeCookie <come.boutevin@gmail.com>
2023-04-28 23:45:20 +0200

    Initialisation d'allegro dans le main.c

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <allegro.h>
+
+#define LONG 800
+#define LARG 600
 
 int main(void) {
-    printf("Hello World!\n");
+    allegro_init();
+    install_keyboard();
+    set_color_depth(desktop_color_depth());
+    if((set_gfx_mode(GFX_AUTODETECT_WINDOWED,(LONG),(LARG),0,0))!=0){
+        allegro_message("Pb de mode graphique");
+        allegro_exit();
+        exit(EXIT_FAILURE);}
+
+    /* Menu → Permet de sélectionner son pseudo et son joueur, le tout est stocké dans une structure Joueur
+     * Le menu lance ensuite la Map principale, cette map doit afficher le numéro du joueur en cours et son
+     * nombre de tickets (rappel à 0 fin de partie) elle lance aussi les mini-jeux → chaque mini jeu est
+     * un sous programme indépendant (appelle les Bitmaps au début et les libère à la fin) → prend en
+     * paramètres la structure joueur pour les tickets
+     * La Map principale doit bien gérer la sortie du sous programme de mini jeu avec le passage au joueur
+     * suivant.
+     * Enfin il faut s'occuper de la victoire et quitter allegro (ça vide la mémoire automatiquement). */
+
+    //menu();
+
+    allegro_exit();
     return 0;
 }
+END_OF_MAIN()
